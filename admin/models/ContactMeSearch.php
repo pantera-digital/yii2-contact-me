e58ed40d10e3f69a3ce3252601356d1c43c9cf3f
singletonn <sopranowest@gmail.com>
2018-07-10 17:33:01 +1000

    Обратная сортировка в админки

--- admin/models/ContactMeSearch.php
+++ admin/models/ContactMeSearch.php
@@ -46,6 +46,9 @@ class ContactMeSearch extends ContactMe
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort' => [
+                'defaultOrder' => ['id' => SORT_DESC],
+            ],
         ]);
 
         $this->load($params);
